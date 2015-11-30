544665c984a5655dd6244c883884c46e1184b6f5
Svalleb043 <svalleb043@C333-WD-432-011.omahaps.ops.org>
2015-11-30 10:34:03 -0600

    lab 9.1

---
+++ Lesson 8/Excercise/9.1.3.py
@@ -0,0 +1,16 @@
+# Brandon Valle
+# 11/16/15
+# Period 5
+
+# 9.1.3
+
+# This program will  check if number is positive or not
+
+num = int(input("Give me a number: "))
+
+if num%2==0:
+    print("Your number is even")
+else:
+    print("Your number is odd")
+
+    
---
+++ Lesson 8/LAB/LAB 9.1.py
@@ -0,0 +1,15 @@
+ Brandon Valle
+# 11/16/15
+# Period 5
+
+# LAB 9.1
+
+# MULTIPLY 13
+
+num = int(input("Give me a number: "))
+
+
+if num%13==0:
+    print("Your number is divisible by 13!")
+else:
+    print("Your number is not divisible by 13!")
